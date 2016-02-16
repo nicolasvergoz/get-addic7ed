78860d6e264664d02ba68124decf10452925add0
Vergoz Nicolas <nicolas.vergoz@gmail.com>
2016-02-16 21:18:08 +0400

    File type argument is not handled by Videofile object anymore

--- lib/addic7ed/videofile.rb
+++ lib/addic7ed/videofile.rb
@@ -4,36 +4,12 @@ module GetAddic7ed
 
     def initialize(file)
       @filepath = File.expand_path(file)
-
-      if self.class.file? (@filepath)
-        @filename = File.basename(@filepath)
-      else
-        raise InvalidVideoFile
-      end
+      @filename = File.basename(@filepath)
     end
 
     def inspect
       puts "Filename".ljust(10) + ": #{@filename}"
       puts "Filepath".ljust(10) + ": #{@filepath}"
-    end
-
-    def self.file? filepath
-      if File.file? (filepath)
-        return true
-      else
-        raise InvalidFile
-      end
-    end
-
-    def self.video? filepath
-      if File.file? (filepath)
-=begin
-     TODO : Find a way to make it work on windows, mac os x and linux
-=end
-        return mimetype(filepath).start_with?("video") || File.extname(filepath) == ".mkv"
-      else
-        raise WrongArgument
-      end
     end
 
   end
